a70a5f44ad03ac39c78a636228ec33fa7040d48f
FKYnJYQ <loveress01@gmail.com>
2022-01-12 14:00:41 +0800

    fix: NeoZoom.lua key-binding.
    Signed-off-by: FKYnJYQ <loveress01@gmail.com>

--- .config/nvim/lua/other-plugins.lua
+++ .config/nvim/lua/other-plugins.lua
@@ -72,8 +72,8 @@ require("toggleterm").setup({
 -- if you use `<CR>` as toggle, then you should handle when to fallback yourself so it won't intercept the plain-old `<CR>`.
 vim.api.nvim_set_keymap(
 	"n",
-	"<CR>",
-	"&ft != 'qf' ? '<cmd>NeoZoomToggle<CR>' : '<CR>'",
+	"<leader>z",
+	":NeoZoomToggle<CR>",
 	{ noremap = true, silent = true, nowait = true }
 )
 
