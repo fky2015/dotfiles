efbf635717723874fb506b269bf4adb8714713eb
FKYnJYQ <loveress01@gmail.com>
2022-01-13 14:40:35 +0800

    fix: unified color scheme.

--- .config/nvim/lua/other-plugins.lua
+++ .config/nvim/lua/other-plugins.lua
@@ -51,22 +51,6 @@ vim.g.indent_blankline_filetype_exclude = "['help', 'startup']"
 
 require("startup").setup({ theme = "dashboard" })
 
-local colors = require("tokyonight.colors").setup()
-
-require("scrollbar").setup({
-	handle = {
-		color = colors.bg_highlight,
-	},
-	marks = {
-		Search = { color = colors.orange },
-		Error = { color = colors.error },
-		Warn = { color = colors.warning },
-		Info = { color = colors.info },
-		Hint = { color = colors.hint },
-		Misc = { color = colors.purple },
-	},
-})
-
 require("toggleterm").setup({
 	open_mapping = [[<c-\>]],
 })
--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,5 +1,24 @@
 -- vim.g.tokyonight_style = "storm"
 
+local github_theme_style = "light"
 require("github-theme").setup({
-	theme_style = "light",
+	theme_style = github_theme_style,
+})
+
+local colors = require("github-theme.colors").setup({
+	theme_style = github_theme_style,
+})
+
+require("scrollbar").setup({
+	handle = {
+		color = colors.bg_highlight,
+	},
+	marks = {
+		Search = { color = colors.orange },
+		Error = { color = colors.error },
+		Warn = { color = colors.warning },
+		Info = { color = colors.info },
+		Hint = { color = colors.hint },
+		Misc = { color = colors.purple },
+	},
 })
