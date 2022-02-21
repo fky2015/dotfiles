10fab6224357b82f2044d109298bcec2199a4591
Feng Kaiyu <loveress01@outlook.com>
2022-02-21 19:29:08 +0800

    fix: text object `argument` support.

--- .config/nvim/lua/fky/treesitter.lua
+++ .config/nvim/lua/fky/treesitter.lua
@@ -30,6 +30,28 @@ require("nvim-treesitter.configs").setup({
 				["if"] = "@function.inner",
 				["ac"] = "@class.outer",
 				["ic"] = "@class.inner",
+				["ia"] = "@parameter.inner",
+				["aa"] = "@parameter.outer",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
 			},
 		},
 	},
