41a9bac83d43b0d91b0f500006333b8342bf00ec
Feng Kaiyu <loveress01@outlook.com>
2022-11-10 09:11:22 +0800

    feat(nvim): add noice.nvim.

--- .config/nvim/lua/fky/plugins.lua
+++ .config/nvim/lua/fky/plugins.lua
@@ -20,6 +20,22 @@ return require("packer").startup({
     -- Packer
     use("wbthomason/packer.nvim")
 
+    use({
+      "folke/noice.nvim",
+      config = function()
+        require("noice").setup({
+        })
+      end,
+      requires = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+      }
+    })
+
     use({ "junegunn/vim-easy-align",
       config = function()
         vim.keymap.set("x", "ga", "<Plug>(EasyAlign)", { noremap = false })
--- .config/nvim/lua/fky/statusline.lua
+++ .config/nvim/lua/fky/statusline.lua
@@ -9,15 +9,20 @@ end
 
 local gps = require("nvim-gps")
 require("lualine").setup({
-	extensions = { "nvim-tree", "fugitive", "quickfix" },
-	sections = {
-		lualine_c = { "filename", { gps.get_location, cond = gps.is_available } },
-		lualine_x = {
-			"encoding",
-			"fileformat",
-			"filetype",
-		},
-		lualine_y = { "%3p%%/%L" },
-	},
-	options = ops,
+  extensions = { "nvim-tree", "fugitive", "quickfix" },
+  sections = {
+    lualine_c = { "filename", { gps.get_location, cond = gps.is_available } },
+    lualine_x = {
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      },
+      "encoding",
+      "fileformat",
+      "filetype",
+    },
+    lualine_y = { "%3p%%/%L" },
+  },
+  options = ops,
 })
