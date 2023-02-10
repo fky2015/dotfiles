208bd33959bed7290666bc7be993bf9c127752ff
Feng Kaiyu <loveress01@outlook.com>
2023-02-10 22:25:07 +0800

    feat: update nvim-tree.

--- .config/nvim/lua/fky/plugins.lua
+++ .config/nvim/lua/fky/plugins.lua
@@ -84,6 +84,7 @@ return require("packer").startup({
 
     use "elkowar/yuck.vim"
 
+    -- https://github.com/petertriho/cmp-git
     use({ "petertriho/cmp-git", requires = "nvim-lua/plenary.nvim" })
 
     use { 'lewis6991/impatient.nvim',
@@ -119,9 +120,6 @@ return require("packer").startup({
         vim.g.tmux_navigator_no_mappings = 1
       end,
     })
-    -- use({ "protex/better-digraphs.nvim", require = {
-    -- 	"nvim-telescope/telescope.nvim",
-    -- } })
 
     use({
       "nyngwang/NeoRoot.lua",
@@ -380,8 +378,70 @@ return require("packer").startup({
         "kyazdani42/nvim-web-devicons", -- optional, for file icon
       },
       config = function()
+        -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+        local function open_nvim_tree(data)
+
+          -- buffer is a [No Name]
+          local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+          -- buffer is a directory
+          local directory = vim.fn.isdirectory(data.file) == 1
+
+          if not no_name and not directory then
+            return
+          end
+
+          -- change to the directory
+          if directory then
+            vim.cmd.cd(data.file)
+          end
+
+          -- open the tree
+          require("nvim-tree.api").tree.open()
+        end
+
+        vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+        -- -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+        -- local function tab_win_closed(winnr)
+        --   local api = require "nvim-tree.api"
+        --   local tabnr = vim.api.nvim_win_get_tabpage(winnr)
+        --   local bufnr = vim.api.nvim_win_get_buf(winnr)
+        --   local buf_info = vim.fn.getbufinfo(bufnr)[1]
+        --   local tab_wins = vim.tbl_filter(function(w) return w ~= winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
+        --   local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
+        --   if buf_info.name:match(".*NvimTree_%d*$") then -- close buffer was nvim tree
+        --     -- Close all nvim tree on :q
+        --     if not vim.tbl_isempty(tab_bufs) then -- and was not the last window (not closed automatically by code below)
+        --       api.tree.close()
+        --     end
+        --   else -- else closed buffer was normal buffer
+        --     if #tab_bufs == 1 then -- if there is only 1 buffer left in the tab
+        --       local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
+        --       if last_buf_info.name:match(".*NvimTree_%d*$") then -- and that buffer is nvim tree
+        --         vim.schedule(function()
+        --           if #vim.api.nvim_list_wins() == 1 then -- if its the last buffer in vim
+        --             vim.cmd "quit" -- then close all of vim
+        --           else -- else there are more tabs open
+        --             vim.api.nvim_win_close(tab_wins[1], true) -- then close only the tab
+        --           end
+        --         end)
+        --       end
+        --     end
+        --   end
+        -- end
+        --
+        -- vim.api.nvim_create_autocmd("WinClosed", {
+        --   callback = function()
+        --     local winnr = tonumber(vim.fn.expand("<amatch>"))
+        --     vim.schedule_wrap(tab_win_closed(winnr))
+        --   end,
+        --   nested = true
+        -- })
+        --
+        -- -- auto close
+
         require("nvim-tree").setup({
-          open_on_setup = true,
           actions = {
             open_file = {
               resize_window = true,
