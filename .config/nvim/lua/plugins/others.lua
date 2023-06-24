b8aa6bdb46cd1bc7a7c2559cb618d0d656c6dfb9
Feng Kaiyu <loveress01@outlook.com>
2023-06-24 18:49:24 +0800

    fix(nvim): better session options.

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -408,6 +408,7 @@ return {
         end
       end
 
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
       require("auto-session").setup {
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/code", "~/Downloads", "/" },
