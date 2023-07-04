1779d60fdbddd3c646bbd41f599ab5f21952482a
Feng Kaiyu <loveress01@outlook.com>
2023-07-04 16:02:38 +0800

    feat: enrich telescope commands.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,19 @@ local k = vim.keymap.set
 -- telescope
 k("n", "<leader>f", "<cmd>Telescope find_files<cr>")
 k("n", "<leader>F", "<cmd>Telescope git_files<cr>")
+local builtin = require("telescope.builtin")
+local utils = require("telescope.utils")
+
+-- ["<leader>ff"] = { function() builtin.find_files({ cwd = utils.buffer_dir() }) end,
+--       desc = "Find files in cwd" }
+k("n", "<leader>FF", function() builtin.find_files({ cwd = utils.buffer_dir() }) end)
 k("n", "<leader>t", "<cmd>Telescope live_grep<cr>")
 k("n", "<leader>b", "<cmd>Telescope buffers<cr>")
 k("n", "<leader>h", "<cmd>Telescope help_tags<cr>")
 k("n", "<leader>s", "<cmd>Telescope treesitter<cr>")
 k("n", "<leader>r", "<cmd>Telescope resume<cr>")
+k("n", "<C-_>", "<cmd>Telescope<cr>")
+k("n", "<leader-/>", "<cmd>Telescope><CR>")
 
 return {
   {
