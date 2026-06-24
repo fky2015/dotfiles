a2fa8b95b3b0ec1ad63cc4df94ddff2c3eab5d11
Feng Kaiyu <fengkaiyu.hi@bytedance.com>
2026-06-24 21:51:21 +0800

    feat(nvim): navigate staged gitsigns hunks with [H and ]H

--- .config/lazynvim/lua/plugins/git.lua
+++ .config/lazynvim/lua/plugins/git.lua
@@ -2,6 +2,26 @@ return {
   { "tpope/vim-fugitive" },
   { "dlyongemallo/diffview.nvim" },
   {
+    "lewis6991/gitsigns.nvim",
+    opts = function(_, opts)
+      local on_attach = opts.on_attach
+
+      opts.on_attach = function(buffer)
+        if on_attach then
+          on_attach(buffer)
+        end
+
+        local gs = package.loaded.gitsigns
+        vim.keymap.set("n", "]H", function()
+          gs.nav_hunk("next", { target = "staged" })
+        end, { buffer = buffer, desc = "Next Staged Hunk", silent = true })
+        vim.keymap.set("n", "[H", function()
+          gs.nav_hunk("prev", { target = "staged" })
+        end, { buffer = buffer, desc = "Prev Staged Hunk", silent = true })
+      end
+    end,
+  },
+  {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = "CodeDiff",
